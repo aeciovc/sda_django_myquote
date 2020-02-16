669a15f8cff95d9d2c456b3d711bddcf128ea56a
Dennis Ast <astasitsev@gmail.com>
2020-02-16 21:30:52 +0200

    Created Buyer model

--- myquote/mainapp/models.py
+++ myquote/mainapp/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Buyer(models.Model):
+    name = models.CharField(max_length=60)
+    email = models.EmailField(max_length=254)
+    id_code = models.CharField(max_length=50)
+    dt_entrance = models.TextField()
+
+    def __str__(self):
+        return "{} ({})".format(self.name, self.id_code)
+
