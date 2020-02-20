4ae87e4f142bbcb086da60779d7966247aabed7b
Dennis Ast <astasitsev@gmail.com>
2020-02-20 15:24:17 +0200

    Created: BuyersUpdateView/BuyersDeleteView/ and all corresponding html templates

--- myquote/buyers/urls.py
+++ myquote/buyers/urls.py
@@ -1,8 +1,11 @@
 from django.urls import path
-from .views import BuyersListView, BuyerCreateView
+from .views import (BuyersListView, BuyersCreateView,
+                    BuyersUpdateView, BuyersDeleteView)
 
 urlpatterns = [
     path('', BuyersListView.as_view(), name="buyers-list"),
-    path('create/', BuyerCreateView.as_view(), name="buyers-create"),
+    path('create/', BuyersCreateView.as_view(), name="buyers-create"),
+    path('update/<int:pk>/', BuyersUpdateView.as_view(), name="buyers-update"),
+    path('delete/<int:pk>/', BuyersDeleteView.as_view(), name="buyers-delete"),
 ]
 
--- myquote/buyers/views.py
+++ myquote/buyers/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.urls import reverse_lazy, reverse
-from django.views.generic import ListView, CreateView
+from django.views.generic import (ListView, CreateView,
+                                  UpdateView, DeleteView)
 from .models import Buyer
 
 
@@ -9,10 +10,26 @@ class BuyersListView(ListView):
     template_name = 'buyers.html'
 
 
-class BuyerCreateView(CreateView):
+class BuyersCreateView(CreateView):
     model = Buyer
     fields = "__all__"
     template_name = 'create_buyers.html'
 
     def get_success_url(self):
         return reverse('buyers-list')
+
+
+class BuyersUpdateView(UpdateView):
+    model = Buyer
+    fields = "__all__"
+    template_name = "update_buyers.html"
+
+    def get_success_url(self):
+        return reverse('buyers-list')
+
+
+class BuyersDeleteView(DeleteView):
+    model = Buyer
+    template_name = "buyer_confirm_delete.html"
+    success_url = reverse_lazy('buyers-list')
+
