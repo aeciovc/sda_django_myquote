ea52e817711efeefe096ebd1314d05d1cfe39c41
Dennis Ast <astasitsev@gmail.com>
2020-02-20 14:14:10 +0200

    Created: buyers.html, BuyersListView

--- myquote/buyers/urls.py
+++ myquote/buyers/urls.py
@@ -1,5 +1,7 @@
 from django.urls import path
+from .views import BuyersListView
 
 urlpatterns = [
-
+    path('', BuyersListView.as_view(), name="buyers-list")
 ]
+
--- myquote/buyers/views.py
+++ myquote/buyers/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
+from django.views.generic import ListView
+from .models import Buyer
 
-# Create your views here.
+
+class BuyersListView(ListView):
+
+    model = Buyer
+    template_name = 'buyers.html'
+
