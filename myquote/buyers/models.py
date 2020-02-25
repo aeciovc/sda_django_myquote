9956a6a4220c50db1cbab0034c73c32c51fea449
Paulo Zacarias <paulo.zacarias@outlook.pt>
2020-02-25 21:30:20 +0200

    Merge with upstream/master

--- myquote/buyers/models.py
+++ myquote/buyers/models.py
@@ -5,7 +5,7 @@ class Buyer(models.Model):
     name = models.CharField(max_length=60)
     email = models.EmailField(max_length=254)
     phone_number = models.BigIntegerField(unique=True)
-    id_code = models.CharField(max_length=50)
+    id_code = models.CharField(max_length=50, blank=True, null=True)
     dt_entrance = models.TextField(blank=True, null=True)
 
     def __str__(self):
---
+++ myquote/buyers/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from django.urls import reverse_lazy, reverse
+from django.views.generic import (ListView, CreateView,
+                                  UpdateView, DeleteView)
+from .models import Buyer
+
+
+class BuyersListView(ListView):
+    model = Buyer
+    template_name = 'buyers.html'
+
+
+class BuyersCreateView(CreateView):
+    model = Buyer
+    fields = "__all__"
+    template_name = 'create_buyers.html'
+
+    def get_success_url(self):
+        return reverse('buyers-list')
+
+
+class BuyersUpdateView(UpdateView):
+    model = Buyer
+    fields = "__all__"
+    template_name = "update_buyers.html"
+
+    def get_success_url(self):
+        return reverse('buyers-list')
+
+
+class BuyersDeleteView(DeleteView):
+    model = Buyer
+    template_name = "buyer_confirm_delete.html"
+    success_url = reverse_lazy('buyers-list')
+
