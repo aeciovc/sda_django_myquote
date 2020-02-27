0bf18fb3477e1d26b7443cec7d0a66e34f094308
Paulo Zacarias <paulo.zacarias@outlook.pt>
2020-02-27 22:30:42 +0200

    Updated field types in product model

--- myquote/products/models.py
+++ myquote/products/models.py
@@ -1,5 +1,10 @@
 from django.db import models
 
+STATUSES = [
+    ('P', 'Published'),
+    ('NP', 'Not published'),
+]
+
 
 class Category(models.Model):
     name = models.CharField(max_length=200)
@@ -9,8 +14,12 @@ class Product(models.Model):
     name = models.CharField(max_length=200)
     description = models.TextField()
     brand = models.CharField(max_length=200)
-    status = models.BooleanField(default=False)
-    image = models.URLField()
+    status = models.CharField(
+        max_length=2,
+        choices=STATUSES,
+        default='NP'
+    )
+    image = models.ImageField(default='default_image.jpg', storage='product_image')
     category = models.ForeignKey(Category, on_delete=models.DO_NOTHING)
 
 
