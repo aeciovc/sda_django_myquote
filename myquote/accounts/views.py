eae3c0de97cdad01740a1133dcdfb2bc1122757b
Dennis Ast <astasitsev@gmail.com>
2020-03-02 20:52:28 +0200

    added functinality to remember 'next' parameter from the url

--- myquote/accounts/views.py
+++ myquote/accounts/views.py
@@ -1,6 +1,6 @@
 from django.contrib.auth import authenticate, login
 from django.http import HttpResponseRedirect, HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.urls import reverse
 
 
@@ -12,9 +12,11 @@ def user_login(request):
         if user:
             if user.is_active:
                 login(request, user)
+                if 'next' in request.POST:
+                    return redirect(request.POST.get('next'))
                 return HttpResponseRedirect(reverse('buyers-list'))
             else:
-                return HttpResponse("Your account was inactive.")
+                return HttpResponse("Your account is inactive.")
         else:
 
             return HttpResponse("Invalid login details given. Go back and try again")
