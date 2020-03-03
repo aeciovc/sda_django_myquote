a63c480aab6c48bd89fea29a83320f048e947aa9
Dennis Ast <astasitsev@gmail.com>
2020-03-03 17:00:52 +0200

    user_signup view and signup.html + routes created

--- myquote/accounts/views.py
+++ myquote/accounts/views.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
+from django.contrib.auth.forms import UserCreationForm
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
 from django.urls import reverse
@@ -32,3 +33,18 @@ def user_logout(request):
     logout(request)
 
     return redirect('index')
+
+
+def user_signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'signup.html', {'form': form})
