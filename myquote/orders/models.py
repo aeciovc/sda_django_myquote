92a6e984dda4a7151e909cf92662797551a709df
Henry Aimre <henryaimre@gmail.com>
2020-03-07 14:21:43 +0200

    Added to Sellers relationship to orders model

--- myquote/orders/models.py
+++ myquote/orders/models.py
@@ -2,14 +2,13 @@ from django.db import models
 
 from myquote.buyers import Buyer
 from myquote.products import Product
-
-
-# from myquote.seller import Seller
+from myquote.seller import Seller
 
 
 class Orders(models.Model):
-    buyer = models.ForeignKey(Buyer, on_delete=None)
-    product = models.ForeignKey(Product, on_delete=None)
+    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
     date_for_product = models.DateTimeField(blank=True)
     comment = models.TextField(blank=None)
 
