706594407abc65ca5e9664bfddb93f82d26fd979
Henry Aimre <henryaimre@gmail.com>
2020-03-07 13:52:31 +0200

    Added orders model

--- myquote/orders/models.py
+++ myquote/orders/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+from myquote.buyers import Buyer
+from myquote.products import Product
+
+
+# from myquote.seller import Seller
+
 # Create your models here.
+
+class Orders(models.Model):
+    buyer = models.ForeignKey(Buyer, on_delete=None)
+    product = models.ForeignKey(Product, on_delete=None)
+    date_for_product = models.DateTimeField(blank=True)
+    comment = models.TextField(blank=None)
+
+    def __str__(self):
+        return self.buyer
