a32b918cdb79d2279ccaf402fc99a57a2e774bfc
Modith hadya leelananda <modith.hadya@gmail.com>
2020-03-07 11:45:42 +0200

    added category views , create , list update and delete

--- myquote/products/urls.py
+++ myquote/products/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
-from .views import CategoryListView
+from .views import CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDetailView, CategoryDeleteView
 
 urlpatterns = [
- path('category_list/', CategoryListView.as_view(), name='category_list')
+ path('category_list/', CategoryListView.as_view(), name='category_list'),
+ path('category_create/', CategoryCreateView.as_view(), name='category_create'),
+ path('category_update/<int:pk>', CategoryUpdateView.as_view(), name='category_update'),
+ path('category_detail/<int:pk>', CategoryDetailView.as_view(), name='category_detail'),
+ path('category_delete/<int:pk>', CategoryDeleteView.as_view(), name='category_delete')
 ]
--- myquote/products/views.py
+++ myquote/products/views.py
@@ -1,7 +1,34 @@
-from django.views.generic import ListView
+from django.views.generic import ListView, CreateView, UpdateView, DetailView, DeleteView
+from django.urls import reverse_lazy
 from .models import Category
 
 class CategoryListView(ListView):
     model = Category
     template_name = 'category_list.html'
+    context_object_name = 'categories'
+
+class CategoryCreateView(CreateView):
+    model = Category
+    template_name = 'category_create.html'
+    fields = '__all__'
+    success_url = reverse_lazy('category_list')
+
+
+class CategoryUpdateView(UpdateView):
+    model = Category
+    template_name = 'category_update.html'
+    fields = ['name']
     context_object_name = 'category'
+    fields = '__all__'
+    success_url = reverse_lazy('category_list')
+
+class CategoryDetailView(DetailView):
+    model = Category
+    template_name = 'category_detail.html'
+    context_object_name = 'category'
+
+class CategoryDeleteView(DeleteView):
+    model = Category
+    template_name = 'category_delete.html'
+    context_object_name = 'category'
+    success_url = reverse_lazy('category_list')
