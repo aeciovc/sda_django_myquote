b7166c62bb4df5d1b39a6e89f89ce3b64d445b7d
Maku <margusrohtla@gmail.com>
2020-03-07 12:32:35 +0200

    Added buyes url to menu items

--- myquote/buyers/urls.py
+++ myquote/buyers/urls.py
@@ -1,12 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.urls import path
-from .views import (BuyersListView, BuyersCreateView,
-                    BuyersUpdateView, BuyersDeleteView)
+from .views import (BuyersListView, BuyersCreateView, BuyersUpdateView, BuyersDeleteView)
 
 urlpatterns = [
     path('', login_required(BuyersListView.as_view()), name="buyers-list"),
     path('create/', login_required(BuyersCreateView.as_view()), name="buyers-create"),
     path('update/<int:pk>/', login_required(BuyersUpdateView.as_view()), name="buyers-update"),
     path('delete/<int:pk>/', login_required(BuyersDeleteView.as_view()), name="buyers-delete"),
+
 ]
 
