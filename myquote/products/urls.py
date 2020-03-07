7f4438005de266553dc87bb0ac6b585fff4cd7ab
Modith hadya leelananda <modith.hadya@gmail.com>
2020-03-07 12:20:34 +0200

    meeged with the Master

--- myquote/products/urls.py
+++ myquote/products/urls.py
@@ -1,7 +1,13 @@
 from django.urls import path
 from .views import CategoryListView, CategoryCreateView, CategoryUpdateView, CategoryDetailView, CategoryDeleteView
+from .views import ProductListView, ProductDetailView, ProductUpdateView, ProductDeleteView, ProductCreateView
 
 urlpatterns = [
+ path('list/', ProductListView.as_view(), name='products_list'),
+ path('detail/<int:pk>', ProductDetailView.as_view(), name='product_detail'),
+ path('update/<int:pk>', ProductUpdateView.as_view(), name='product_update'),
+ path('delete/<int:pk>', ProductDeleteView.as_view(), name='product_delete'),
+ path('add/', ProductCreateView.as_view(), name='product_create'),
  path('category_list/', CategoryListView.as_view(), name='category_list'),
  path('category_create/', CategoryCreateView.as_view(), name='category_create'),
  path('category_update/<int:pk>', CategoryUpdateView.as_view(), name='category_update'),
