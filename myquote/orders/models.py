37e819b8e12e0d7e6291d28ae121526eb86184cb
Henry Aimre <henryaimre@gmail.com>
2020-03-07 12:45:24 +0200

    Created new app: orders

--- myquote/orders/models.py
+++ myquote/orders/models.py
@@ -1,16 +1,3 @@
 from django.db import models
 
-from myquote.buyers import Buyer
-from myquote.products import Product
-from myquote.seller import Seller
-
-
-class Orders(models.Model):
-    buyer = models.ForeignKey(Buyer, on_delete=models.CASCADE)
-    product = models.ForeignKey(Product, on_delete=models.CASCADE)
-    seller = models.ForeignKey(Seller, on_delete=models.CASCADE)
-    date_for_product = models.DateTimeField(blank=True)
-    comment = models.TextField(blank=None)
-
-    def __str__(self):
-        return self.buyer
+# Create your models here.
