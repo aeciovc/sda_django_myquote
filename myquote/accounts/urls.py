62f1541b8c0386b6e14dcb0f860405699c733500
Dennis Ast <astasitsev@gmail.com>
2020-03-08 10:50:48 +0200

    templated for signup is ready to choose a registration for buyer/seller

--- myquote/accounts/urls.py
+++ myquote/accounts/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
+from django.views.generic import TemplateView
 
 from .views import user_login, user_logout, user_signup_for_buyers
 
 urlpatterns = [
     path('login/', user_login, name="user-login"),
     path('logout/', user_logout, name="user-logout"),
-    path('signup/buyer', user_signup_for_buyers, name="user-signup-buyer"),
+    path('signup/buyer/', user_signup_for_buyers, name="user-signup-buyer"),
+    path('signup/', TemplateView.as_view(template_name='seller_buyer_signup_screen.html'), name="user-signup"),
 ]
 
