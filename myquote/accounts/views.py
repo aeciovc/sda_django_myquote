2c185a3d9d141290d79eae5a99cd2687dd084a3d
Dennis Ast <astasitsev@gmail.com>
2020-03-08 12:13:40 +0200

    Created registration option for Seller

--- myquote/accounts/views.py
+++ myquote/accounts/views.py
@@ -3,6 +3,7 @@ from django.contrib.auth.decorators import login_required
 from django.contrib.auth.models import User
 
 from buyers.models import Buyer
+from seller.models import Seller
 from .forms import SignUpForm
 from django.http import HttpResponseRedirect, HttpResponse
 from django.shortcuts import render, redirect
@@ -56,6 +57,34 @@ def user_signup_for_buyers(request):
             dt_entrance = form.cleaned_data.get('dt_entrance')
             buyer_profile = Buyer(user=our_user, phone_number=phone_number, id_code=id_code, dt_entrance=dt_entrance)
             buyer_profile.save()
+
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = SignUpForm()
+    return render(request, 'signup.html', {'form': form})
+
+
+def user_signup_for_seller(request):
+
+    if request.user.is_authenticated:
+        return redirect('index')
+
+    if request.method == 'POST':
+        form = SignUpForm(request.POST)
+        if form.is_valid():
+            form.save()
+
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+
+            our_user = User.objects.get(username=username)
+            phone_number = form.cleaned_data.get('phone')
+            id_code = form.cleaned_data.get('id_code')
+            dt_entrance = form.cleaned_data.get('dt_entrance')
+            seller_profile = Seller(user=our_user, phone_number=phone_number, id_code=id_code, dt_entrance=dt_entrance)
+            seller_profile.save()
 
             user = authenticate(username=username, password=raw_password)
             login(request, user)
